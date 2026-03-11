f7bc118cf29026b87169465dd084db4cee21f1b9
dhanashri-bhopale <bhopaledhanashri@gmail.com>
2026-03-11 21:59:05 +0530

    corrected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   BASE_PRODUCTS_URL : 'https://dummyjson.com',
-  BASE_SHOWs_URL : 'https://api.tvmaze.com'
+  BASE_SHOWS_URL : 'https://api.tvmaze.com'
 };
